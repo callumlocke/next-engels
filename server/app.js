928432db6519a24d3a7790f035fa3d6925570f0e
Matthew Andrews <matt@mattandre.ws>
2014-10-07 16:49:21 +0100

    Add comment

--- server/app.js
+++ server/app.js
@@ -1,3 +1,4 @@
+// TODO - Make sure this is defined by the ft-api-client - shouldn't be needed here
 require('es6-promise').polyfill();
 var express = require('express');
 var swig = require('swig');
