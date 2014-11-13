66d1da41312e00bb876055638087e114a5304e4c
Rhys Evans <rhys.evans@ft.com>
2014-11-13 13:01:05 +0000

    local copies of fixtures

--- tests/server/smokeSpec.js
+++ tests/server/smokeSpec.js
@@ -9,9 +9,9 @@ var nock = require('nock');
 var request = require('request');
 var server;
 
-var article = require('fs').readFileSync('node_modules/ft-api-client/test/fixtures/03b49444-16c9-11e3-bced-00144feabdc0', { encoding: 'utf8' });
+var article = require('fs').readFileSync('tests/fixtures/03b49444-16c9-11e3-bced-00144feabdc0', { encoding: 'utf8' });
 var recommends = require('fs').readFileSync('tests/fixtures/recommends.xml', { encoding: 'utf8' });
-var page = require('fs').readFileSync('node_modules/ft-api-client/test/fixtures/page_front-page', { encoding: 'utf8' });
+var page = require('fs').readFileSync('tests/fixtures/page_front-page', { encoding: 'utf8' });
         
 var servesGoodHTML = function (url, done) {
     request
