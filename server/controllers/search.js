f5a59c9f6732cbae3c4e70d109fa19ef285e940d
Matt Chadburn <matt.chadburn@ft.com>
2014-11-25 15:29:39 +0000

    frontPage -> topStories

--- server/controllers/search.js
+++ server/controllers/search.js
@@ -3,8 +3,8 @@
 var Search = require('../jobs/search');
 
 // Periodically load these searches in to memory
-var frontPage = new Search();
-frontPage.init('page:Front page', 10);
+var topStories = new Search();
+topStories.init('page:Front page', 10);
 
 var bigRead = new Search();
 bigRead.init('page:The Big Read', 5);
@@ -15,7 +15,7 @@ comment.init('page:comment', 5);
 module.exports = function(req, res) {
     res.render('layout', {
         streams: [
-            { related: [], items: frontPage.stream.items, meta: [], title: 'Top stories' },
+            { related: [], items: topStories.stream.items, meta: [], title: 'Top stories' },
             { related: [], items: bigRead.stream.items, meta: [], title: 'FT Highlights' },
             { related: [], items: comment.stream.items, meta: [], title: 'Comment & columnists' }
         ],
