426d7c0085918d5c1fdfb811bf8a8f4cd158ce86
Matt Andrews <matt@mattandre.ws>
2014-11-27 20:18:41 +0000

    Recommend me not

--- client/main.js
+++ client/main.js
@@ -10,20 +10,6 @@ swig.setFilter('resize', require('../templates/helpers/resize'));
 
 var tile = require('../templates/components/tile.html');
 
-if (FT.flags.contentApiCalls.isSwitchedOn) {
-
-    try {
-    $.getJSON('/engels/recommended?eid='+document.cookie.match(/_EID=([0-9]+)_/)[1])
-        .then(function(data) {
-            var el = document.querySelector('.recommended');
-            el.innerHTML = '<ul>' + data.map(function(item) {
-                return swig.render(tile, {locals: {article: item, isInList: true, isLarge: false}});
-            }).join('') + '</ul>';
-            document.dispatchEvent(new CustomEvent('o.DOMContentLoaded'));
-        });
-    } catch (e) {}
-}
-
 if (FT.flags.ads.isSwitchedOn) {
     //Fixed position ads:
     window.jQuery = require('jquery');
