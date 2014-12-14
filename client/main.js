2ab83304f31c2f4c1e3e8f33dd3be2ede15aa70b
Matthew Andrews <matt@mattandre.ws>
2014-12-14 11:34:39 +0000

    Upgrade final bit of deprecated flags logic; fix build

--- client/main.js
+++ client/main.js
@@ -1,20 +1,22 @@
 require('ft-next-wrapper');
 require('next-article-card-component');
+var flags = require('next-feature-flags-client');
 
 document.querySelector('[data-o-component="o-header"]').setAttribute('data-panel', 'search');
 var $ = require('jquery');
 
-var tile = require('../templates/components/tile.html');
-
-if (FT.flags.ads.isSwitchedOn) {
-    //Fixed position ads:
-    window.jQuery = require('jquery');
-    var filamentFixed = require('filament-fixed');
-    var filamentSticky = require('filament-sticky');
-    //Display ads: fixed-sticky plugin
-    FixedSticky.tests.sticky = false;
-    var ads = document.querySelectorAll('[data-display-ad]');
-    for(var i = 0; i < ads.length; i++){
-        $(ads[i]).fixedsticky();
-    }    
-}
+flags.init()
+	.then(function() {
+		if (flags.get('ads').isSwitchedOn) {
+			//Fixed position ads:
+			window.jQuery = require('jquery');
+			var filamentFixed = require('filament-fixed');
+			var filamentSticky = require('filament-sticky');
+			//Display ads: fixed-sticky plugin
+			FixedSticky.tests.sticky = false;
+			var ads = document.querySelectorAll('[data-display-ad]');
+			for (var i = 0; i < ads.length; i++) {
+				$(ads[i]).fixedsticky();
+			}
+		}
+	});
