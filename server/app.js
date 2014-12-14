12cbad95a4b433d7c22e4153cf99a86beeedc19c
Matthew Andrews <matt@mattandre.ws>
2014-12-14 21:40:39 +0000

    Upgrade ft-next-express

--- server/app.js
+++ server/app.js
@@ -8,7 +8,6 @@ Metrics.init({ app: 'engels', flushEvery: 30000 });
 
 var port = process.env.PORT || 3001;
 var app = module.exports = express();
-app.locals.name = 'engels';
 
 app.set('views', __dirname + '/../templates');
 
